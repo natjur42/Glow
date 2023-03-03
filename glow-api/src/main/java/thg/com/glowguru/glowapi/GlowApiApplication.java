0171749a6df4a820742051987248428e31fd54ca
Natalia.Jurcakova <natalia.jurcakova42@thehutgroup.com>
2023-03-03 12:07:38 +0000

    added springboot project - backend

---
+++ glow-api/src/main/java/thg/com/glowguru/glowapi/GlowApiApplication.java
@@ -0,0 +1,13 @@
+package thg.com.glowguru.glowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlowApiApplication.class, args);
+    }
+
+}
---
+++ glow-api/src/test/java/thg/com/glowguru/glowapi/GlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package thg.com.glowguru.glowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
